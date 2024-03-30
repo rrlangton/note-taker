14e2de09acb821268029fc496742a4fd6855821e
Robin <rrlangton@gmail.com>
2024-03-29 23:00:53 -0500

    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const api = require('./Develop/public/assets/js/index.js');
+const api = require('./Develop/public/assets/js/index');
 const { randomUUID } = require('crypto');
 
 const PORT = process.env.PORT || 3001;
