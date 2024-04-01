9f62e60c6834b329fc0ee7ebc9eb0aed08bd49af
Robin <rrlangton@gmail.com>
2024-03-31 19:35:12 -0500

    debugging

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -16,6 +16,7 @@ const readAndAppend = (content, file) => {
             const parsedData = JSON.parse(data);
             parsedData.push(content);
             writeToFile(file, parsedData);
+            return parsedData;
         };
     });
 };
--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const path = require('path');
-const api = require('./Develop/public/assets/js/index');
-const { randomUUID } = require('crypto');
-
+const { v4: uuidv4 } = require('uuid');
 const PORT = process.env.PORT || 3001;
-
+const {readFromFile, readAndAppend} = require('./helpers/fsUtils');
 const app = express();
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 app.use(express.static('public'));
 
 // note path to database
@@ -18,7 +15,8 @@ const notePath = path.resolve(__dirname, 'db', 'db.json');
 
 // GET route for notes 
 app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+    readFromFile ('./db/db.json').then(note => {
+        res.json(JSON.parse(note))})
 });
 
 // POST route for notes
@@ -30,22 +28,23 @@ app.post('/api/notes', (req, res) => {
             const newNote = {
                 title,
                 text,
-                note_id: uuidv4(),
+                id: uuidv4(),
             };
 
-            readAndAppend(newNote, './Develop/db/db.json');
-            res.json('Successfully recorded the note');
+            const parsedData = readAndAppend(newNote, './db/db.json');
+            res.json(parsedData);
         } else {
             res.error('Please include a title and text');
         };
 });
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
 // wildcard GET route to index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 app.listen(PORT, () =>
 console.log(`App listening at http://localhost:${PORT}`));
-
-module.exports = notes;
